b5086a6593474d516d2038fbdb4e78f0f95d7523
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-05 11:23:40 -0700

    Init Commit

---
+++ test/e2e/_db.js
@@ -0,0 +1,11 @@
+/* CONNECT TO DB */
+process.env.MONGODB_URI = 'mongodb://localhost:27017/hoppy-test';
+require('../../lib/connect');
+const connection = require('mongoose').connection;
+
+//export a small helper for dropping DB
+module.exports = {
+  drop() {
+    return connection.dropDatabase();
+  }
+};
