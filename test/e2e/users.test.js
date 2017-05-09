6f3c419cf5fc500daca7dac8640d8556bb7e3d6f
Ivy Patton <lilaivy@gmail.com>
2017-05-08 17:39:13 -0700

    post test in users passing

--- test/e2e/users.test.js
+++ test/e2e/users.test.js
@@ -13,6 +13,53 @@ describe('hops api', () => {
                 const users = res.body;
                 assert.deepEqual(users, []);
             });
-
     });
+
+    let fakeUser1 = {
+        name: 'fake',
+        email: 'fakeUser1@fake.com',
+        password: 'fakePassword',
+        friends: [],
+        faveGenre: ['acoustic', 'afrobeat', 'alt-rock']
+
+    };
+    // let fakeUser2 = {
+    //     name: 'fake2',
+    //     email: 'fakeUser2@fake.com',
+    //     password: 'fakePassword',
+    //     friends: ['fakeUser1'],
+    //     faveGenre: ['acoustic', 'afrobeat', 'alt-rock'],
+    // };
+
+    // let fakeUser3 = {
+    //     name: 'fake3',
+    //     email: 'fakeUser3@fake.com',
+    //     password: 'fakePassword',
+    //     friends: ['ivy', 'chris'],
+    //     faveGenre: ['acoustic', 'afrobeat', 'alt-rock'],
+    // };
+
+    function saveUser(user) {
+        return request
+            .post('/users')
+            .send(user)
+            .then(res => res.body);
+    }
+
+    it('rountrips a new user', () => {
+        return saveUser(fakeUser1)
+            .then(savedUser => {
+                assert.ok(savedUser._id, 'saved has id');
+                fakeUser1 = savedUser;
+            });
+            // .then(() => {
+            //     return request.get(`/users/${fakeUser1._id}`);
+            // })
+            // .then(res => res.body)
+            // .then(gotUser => {
+            //     assert.deepEqual(gotUser, fakeUser1);
+            // });
+    });
+
+
 });
