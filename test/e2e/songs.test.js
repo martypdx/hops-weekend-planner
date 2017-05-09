25b55c5f1f25a29719a9808ca45f4ec757e605ea
Ivy Patton <lilaivy@gmail.com>
2017-05-09 11:52:58 -0700

    it updates songs e2e

--- test/e2e/songs.test.js
+++ test/e2e/songs.test.js
@@ -19,19 +19,19 @@ describe('songs api', () => {
         title: 'Halo',
         artist: 'Beyonce',
         swipeLeft: false,
-        spotifyID: '5DGJC3n9DS0Y9eY5ul9y0O'
+        spotifyId: '5DGJC3n9DS0Y9eY5ul9y0O'
     };
     let fakeSong2 = {
         title: 'Lame Song',
         artist: 'Train',
         swipeLeft: true,
-        spotifyID: '5DGJC3n9DS0Y9eY5ul9y05'
+        spotifyId: '5DGJC3n9DS0Y9eY5ul9y05'
     };
     let fakeSong3 = {
         title: 'List',
         artist: 'Kenrick Lamar',
         swipeLeft: false,
-        spotifyID: '5DGJC3n9DS0Y9eY7ul9y0O'
+        spotifyId: '5DGJC3n9DS0Y9eY7ul9y0O'
     };
 
     function saveSong(song) {
@@ -65,6 +65,46 @@ describe('songs api', () => {
                 assert.equal(res.status, 404);
             }
             );
-
     });
+
+    it('returns list of all songs', () => {
+        return Promise.all([
+            saveSong(fakeSong2),
+            saveSong(fakeSong3)
+        ])
+            .then(savedSong => {
+                fakeSong2 = savedSong[0];
+                fakeSong3 = savedSong[1];
+            })
+            .then(() => request.get('/songs'))
+            .then(res => res.body)
+            .then(songs => {
+                assert.equal(songs.length, 3);
+                function test(fakeSong) {
+                    console.log('******songs', songs);
+                    assert.include(songs, {
+                        title: fakeSong.title,
+                        artist: fakeSong.artist,
+                        _id: fakeSong._id,
+                        spotifyId: fakeSong.spotifyId,
+                        swipeLeft: fakeSong.swipeLeft,
+                    });
+                }
+
+                test(fakeSong1);
+                test(fakeSong2);
+                test(fakeSong3);
+            });
+    });
+
+    it('updates songs', () => {
+        fakeSong3.title = 'updated fake song';
+        return request.put(`/songs/${fakeSong3._id}`)
+            .send(fakeSong3)
+            .then(res => res.body)
+            .then(updated => {
+                assert.equal(updated.title, 'updated fake song');
+            });
+    });
+
 });
